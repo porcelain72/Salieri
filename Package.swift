f470f656f1826b7f4b2713a01911381628525608
Peter Macdonald <porcelain2015@gmail.com>
2025-07-28 09:39:17 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Salieri",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Salieri",
+            targets: ["Salieri"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Salieri"),
+        .testTarget(
+            name: "SalieriTests",
+            dependencies: ["Salieri"]),
+    ]
+)
