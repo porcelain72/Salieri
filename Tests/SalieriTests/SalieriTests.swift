e1abd97a62025b59a1c92ca85e739a0da0f255ec
Peter Macdonald <porcelain2015@gmail.com>
2025-07-28 10:20:31 +0100

    Fix MIDIMetaEvent.data tuple construction in tests: ensure all elements are UInt8 and tuple is 32 elements long for time/key signature meta events.

--- Tests/SalieriTests/SalieriTests.swift
+++ Tests/SalieriTests/SalieriTests.swift
@@ -30,15 +30,31 @@ final class SalieriTests: XCTestCase {
         MusicSequenceNewTrack(seq, &track)
         guard let trk = track else { XCTFail("Failed to create track"); return }
         // Add time signature meta event (3/4)
-        var timeSig: [UInt8] = [3, 2, 24, 8] // numerator=3, denominator=2^2=4
-        timeSig.withUnsafeBufferPointer { ptr in
-            var meta = MIDIMetaEvent(metaEventType: 0x58, unused1: 0, unused2: 0, unused3: 0, dataLength: 4, data: (ptr[0], ptr[1], ptr[2], ptr[3], 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+        var timeSig: [UInt8] = [3, 2, 24, 8]
+        var timeSigData = [UInt8](repeating: 0, count: 32)
+        for i in 0..<timeSig.count { timeSigData[i] = timeSig[i] }
+        timeSigData.withUnsafeBufferPointer { ptr in
+            let tuple = (
+                ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6], ptr[7],
+                ptr[8], ptr[9], ptr[10], ptr[11], ptr[12], ptr[13], ptr[14], ptr[15],
+                ptr[16], ptr[17], ptr[18], ptr[19], ptr[20], ptr[21], ptr[22], ptr[23],
+                ptr[24], ptr[25], ptr[26], ptr[27], ptr[28], ptr[29], ptr[30], ptr[31]
+            )
+            var meta = MIDIMetaEvent(metaEventType: 0x58, unused1: 0, unused2: 0, unused3: 0, dataLength: 4, data: tuple)
             MusicTrackNewMetaEvent(trk, 0.0, &meta)
         }
         // Add key signature meta event (2 sharps, major)
         var keySig: [UInt8] = [2, 0]
-        keySig.withUnsafeBufferPointer { ptr in
-            var meta = MIDIMetaEvent(metaEventType: 0x59, unused1: 0, unused2: 0, unused3: 0, dataLength: 2, data: (ptr[0], ptr[1], 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+        var keySigData = [UInt8](repeating: 0, count: 32)
+        for i in 0..<keySig.count { keySigData[i] = keySig[i] }
+        keySigData.withUnsafeBufferPointer { ptr in
+            let tuple = (
+                ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6], ptr[7],
+                ptr[8], ptr[9], ptr[10], ptr[11], ptr[12], ptr[13], ptr[14], ptr[15],
+                ptr[16], ptr[17], ptr[18], ptr[19], ptr[20], ptr[21], ptr[22], ptr[23],
+                ptr[24], ptr[25], ptr[26], ptr[27], ptr[28], ptr[29], ptr[30], ptr[31]
+            )
+            var meta = MIDIMetaEvent(metaEventType: 0x59, unused1: 0, unused2: 0, unused3: 0, dataLength: 2, data: tuple)
             MusicTrackNewMetaEvent(trk, 0.0, &meta)
         }
         let score = SalieriScore.from(sequence: seq)
